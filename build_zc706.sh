2f621264283bfa29ece903ed81bf40ff3ec9c77b
john-susitronix <john@susitronix.com>
2022-01-12 13:36:11 -0800

    automated SD card and warning

--- build_zc706.sh
+++ build_zc706.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+usesd=1
+
 pushd etharb_tx
 vivado_hls script.tcl
 popd
@@ -13,11 +15,17 @@ vivado -mode batch -source zc706_etharb.tcl
 popd
 
 pushd linux
-petalinux-create -t project -n linux_zc706 --template zynq
+petalinux-create -t project -n linux_zc706 --template zynq --force
 pushd linux_zc706
 cp ../../etharb_test/zc706_etharb/zc706_etharb.runs/impl_1/main.sysdef ../../etharb_test/zc706_etharb/zc706_etharb.runs/impl_1/main.hdf
 petalinux-config --get-hw-description=../../etharb_test/zc706_etharb/zc706_etharb.runs/impl_1/ --silentconfig
-echo "I normally will configure for SD debian here"
+
+if [ $USESD -gt 0 ]; then
+    printf "\n\nWARNING: will be using 2nd partition as rootfs\n\n\n"
+    sleep 1
+    cp ../config_sdcard project-spec/configs/config
+fi
+
 cp ../system-user.dtsi project-spec/meta-user/recipes-bsp/device-tree/files/system-user.dtsi
 cp -r ../recipes-kernel project-spec/meta-user/
 petalinux-build
